d87bf5f8ad32ad4f0501bf3b1f8a1ec8a3561d61
Pau <113383428+daw2-diaz23@users.noreply.github.com>
2023-06-03 10:28:19 -0700

    Add files via upload

---
+++ chat/class.pdofactory.php
@@ -0,0 +1,17 @@
+<?php
+global $PDOS;
+
+
+class PDOFactory {
+    public static function GetPDO( $strDSN, $strUser, $strPass, $arParms) {
+        
+        $strKey = md5(serialize(array( $strDSN, $strUser, $strPass, $arParms)));
+        if(!isset($GLOBALS["PDOS"][ $strKey])) $GLOBALS["PDOS"][ $strKey]=0;
+        if (!( $GLOBALS["PDOS"][ $strKey] instanceof PDO)) {
+            $GLOBALS["PDOS"][ $strKey] = new PDO( $strDSN, $strUser, $strPass, $arParms);
+        };
+        
+        return($GLOBALS["PDOS"][$strKey]);
+    }
+}
+?>
